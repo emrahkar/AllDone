d849c343a3e8a31410973cfca891d2027cd28892
Emrah <emrahkbulut@gmail.com>
2022-06-20 14:40:20 +0300

    Setup Project Done

---
+++ AllDone/AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by Emrah Karabulut on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
