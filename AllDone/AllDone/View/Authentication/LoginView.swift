9f96530260d38078a4d8407a028edb5c2e6264d3
Emrah <emrahkbulut@gmail.com>
2022-06-21 19:54:52 +0300

    Email textfield is added for login

--- AllDone/AllDone/View/Authentication/LoginView.swift
+++ AllDone/AllDone/View/Authentication/LoginView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         ZStack{
             BackgroundGradientView()
             
             VStack{
                 LogoView()
+                    .padding(.bottom, 25)
+                
+                VStack(spacing: 20){
+                    EmailTextField(text: $email)
+                        .padding(.horizontal, 32)
+                }
             }
         }
     }
