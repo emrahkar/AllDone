e0e68de4315cb1d882f9a41802f506a89574aa4a
Emrah <emrahkbulut@gmail.com>
2022-06-22 17:04:29 +0300

    Forgot Password and Aign In buttons are added to LoginView

--- AllDone/AllDone/View/Authentication/LoginView.swift
+++ AllDone/AllDone/View/Authentication/LoginView.swift
@@ -25,6 +25,30 @@ struct LoginView: View {
                     PasswordSecureField(text: $password, placeholder: Text("Password"))
                 }
                 .padding(.horizontal, 32)
+                
+                HStack{
+                    Spacer()
+                    
+                    Button {
+                        //Forgot Password Action
+                    } label: {
+                        Text("Forgot Password")
+                            .foregroundColor(.white)
+                            .font(.system(size: 13, weight: .semibold))
+                            .padding(.top)
+                            .padding(.trailing, 28)
+                    }
+                }
+                
+                Button {
+                    //Sign In Action
+                } label: {
+                    AuthenticateButtonView(text: "Sign In")
+                        .padding()
+                }
+
+                Spacer()
+                
             }
         }
     }
